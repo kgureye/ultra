c03f3e69232d57e7569e6cbf83ae49cb86e8b0e7
Kani <kanigureye@outlook.com>
2020-10-13 00:31:08 +0200

    "Finished adding globalstyles"

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
-
+import GlobalStyle from './globalStyles';
+import {  BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import { Navbar } from './components';
 
 
 function App() {
   return (
-    <div className="App">
-      <h1>What's up</h1>
-    </div>
+    <Router>
+      <GlobalStyle />
+        <Navbar />
+    </Router>
+   
+
   );
 }
 
