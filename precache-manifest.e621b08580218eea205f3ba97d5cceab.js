44738c3a48420516b20296bc5333186adbfc610a
kgureye <48024387+kgureye@users.noreply.github.com>
2022-09-08 19:17:37 +0200

    Updates

---
+++ precache-manifest.e621b08580218eea205f3ba97d5cceab.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7aeb00cbc851b2a900903dd2d478e4a9",
+    "url": "/ultra/index.html"
+  },
+  {
+    "revision": "7814386e489f1144a327",
+    "url": "/ultra/static/js/2.59c0fb3f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ultra/static/js/2.59c0fb3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e8672aa1d5dca1384be",
+    "url": "/ultra/static/js/main.4bfcd10a.chunk.js"
+  },
+  {
+    "revision": "82471e2dace16b8a2040",
+    "url": "/ultra/static/js/runtime-main.2f4bf674.js"
+  },
+  {
+    "revision": "79ba2002d56019901132986d5c84067a",
+    "url": "/ultra/static/media/profile.79ba2002.jpg"
+  },
+  {
+    "revision": "55cfb4817b9e054491bd9dafc21087c2",
+    "url": "/ultra/static/media/svg-1.55cfb481.svg"
+  },
+  {
+    "revision": "d4846e1163a89864dd49fb03ec6308ed",
+    "url": "/ultra/static/media/svg-2.d4846e11.svg"
+  },
+  {
+    "revision": "397c455a49d4f872c292a3dc25432579",
+    "url": "/ultra/static/media/svg-3.397c455a.svg"
+  }
+]);
